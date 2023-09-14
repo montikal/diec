879c97c60b0ab75658342ec979b3706edacc448b
montikal <40648651+montikal@users.noreply.github.com>
2023-09-14 18:16:12 +0800

    Delete day3 directory

--- day3/docker/build_docker.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker build -t='mont43/rpi-buster-tf2.0:1.1' .
--- day3/docker/launch_docker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-docker run -it \
-    -v $HOME/diec:/code \
-    mont43/rpi-buster-tf2.0:1.1
